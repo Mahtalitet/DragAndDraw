bd848f064562dab06ab2b9715c5ffaf7f70f8cbe
ilya <MAAFI4I@gmail.com>
2016-06-12 19:47:34 +0200

    Added log messages to see actions

--- app/src/main/java/com/learning/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/learning/draganddraw/BoxDrawingView.java
@@ -2,9 +2,12 @@ package com.learning.draganddraw;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class BoxDrawingView extends View {
+    private static final String TAG = BoxDrawingView.class.toString();
 
     public BoxDrawingView(Context context) {
         this(context, null);
@@ -12,5 +15,27 @@ public class BoxDrawingView extends View {
 
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "ACTION_DOWN: User pressed at the screen.");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TAG, "ACTION_MOVE: User is moving by the screen.");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "ACTION_UP: User put away a finger from the screen.");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "ACTION_CANCEL: Parent view intercept an action event.");
+                break;
+        }
+
+        return true;
     }
 }
